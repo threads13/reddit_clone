77065a01a38a10b46574e15f023e8e623dacfe6e
Jacob Hoke <jacob@threads13.com>
2018-02-15 21:59:14 -0500

    added flexbox and basic lay out to main component

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -4,8 +4,7 @@ import './list.css';
 const List = props => {
   return (
     <div className="list">
-      <p className="left-test">List Component</p>
-      <p className="sidebar-test">List Component 3</p>
+      List Component
     </div>
   )
 }
