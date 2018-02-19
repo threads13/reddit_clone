8231dc73731f79f72034333c90dcd2c998169ba2
Jacob Hoke <jacob@threads13.com>
2018-02-18 20:20:03 -0500

    styled margins and borders.  set up correct colors and font sizes.

--- src/components/list/post_item.js
+++ src/components/list/post_item.js
@@ -5,21 +5,22 @@ import './post_item.css';
 
 const PostItem = props => {
   return (
-    <div className="post-item" style={{marginTop: '20px'}}>
-      <p style={{color: '#c05b4d', fontSize: '20px'}}>
+    <div className="post-item" style={{marginTop: '35x'}}>
+      <p style={{color: '#c05b4d', fontSize: '20px', marginBottom: '4px'}}>
         {props.data.title}
       </p>
-      <p style={{fontSize: '10px'}}
+      <p style={{fontSize: '11px', color: '#b3b3b3', marginBottom: '4px'}}
         >
-        Submitted X hours ago by:  {props.data.author}
+        Submitted X hours ago by:  <span style={{color: 'black'}}>{props.data.author}</span>
       </p>
-      <div style={{marginBottom: '50px'}} className="actions">
-        <a style={{color: '#c05b4d'}} href="">comment</a>
+      <div style={{marginBottom: '35px',}} className="actions">
+        <a style={{color: '#c05b4d'}} href="">{props.data.num_comments} comments</a>
         <a href="">share</a>
         <a href="">save</a>
         <a href="">hide</a>
         <a href="">report </a>
       </div>
+      <hr />
     </div>
   );
 }
