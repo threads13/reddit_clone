24056b915c37796becf336f8c0fe50fe8bbecc90
Jacob Hoke <jacob@threads13.com>
2018-03-14 22:48:53 -0400

    Changed class component to stateless functional component.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import Navbar from './components/navbar';
 import Main from './components/main';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <div>
-          <Main />
-        </div>
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <div>
+        <Main />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default App;
